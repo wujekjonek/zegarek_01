f203de4d091c7635962d1b74ea2f0ba9eaba13f3
jonasz.kozlowski <termometr@poczta.onet.pl>
2017-09-27 15:25:20 +0200

    z qnt

--- main.ts
+++ main.ts
@@ -1,16 +1,23 @@
-console.log("skahfgASFGAFGJKAG");
+var godziny = document.getElementById("godziny");
+var godzinyspot = document.getElementById("godzinyspot");
 
 
-let i;
-for (i = 0; i < 13; i++) {
-    console.log("--> "+ i);
+function ktoraGodzina() {
+    var n = new Date().getHours();
+    document.getElementById("aktualnaGodzina").innerHTML = n;
+    document.getElementById("godzina").innerHTML = n;
+    godziny.setAttribute("transform", "rotate(" + ((n * 30) + 90) + ", 150, 250)");
+    godzinyspot.setAttribute("transform", "rotate(" + ((n * 30) + 90) + ", 150, 250)");
 }
+
+
+ktoraGodzina();
 
 
 function okno() {
-    let x = document.getElementById("godzina");
+    var x = document.getElementById("godzina");
     x.innerHTML = (window.event.target.id);
-//    alert(event.srcElement.id);
-    console.log(window.event.target.id)
+    console.log(window.event.target.id);
+    godzinyspot.style.display = 'none';
+    godziny.setAttribute("transform", "rotate(" + ((window.event.target.id * 30) + 90) + ", 150, 250)");
 }
-
